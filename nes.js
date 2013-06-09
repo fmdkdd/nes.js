c2f48a466b40b9c75d6dfa9998cb6201ddab4b52
FMdKdD <fmdkdd@gmail.com>
2013-06-09 16:20:42 +0200

    Tie everything together

---
+++ nes.js
@@ -0,0 +1,58 @@
+(function(global) {
+
+	"use strict";
+
+	document.addEventListener('DOMContentLoaded', function() {
+		loadLogTrace(function(logTrace) {
+			loadNESFile(function(nesRom) {
+				testCpu(nesRom, logTrace);
+			});
+		});
+	});
+
+	function startEmulation(nesRom) {
+		var cpu = global.cpu;
+		cpu.memory = global.memory;
+
+		cpu.memory.loadROM(nesRom);
+
+		var running = true;
+		var totalCycleCount = 0;
+		var cycleLimit = 100;
+
+		cpu.reset();
+
+		while (running) {
+			var cycles = cpu.step();
+			totalCycleCount += cycles;
+
+			if (totalCycleCount > cycleLimit) {
+				throw new Error("Cycle limit exceeded");
+			}
+		}
+	}
+
+	function loadNESFile(callback) {
+		var rom = document.querySelector('rom');
+		fetch(rom.getAttribute('src'), function(arrayBuffer) {
+			callback(decodeNES(arrayBuffer));
+		})
+	}
+
+	function fetch(url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.responseType = "arraybuffer";
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status <= 200) {
+					console.log('Loaded ', url);
+					callback(xhr.response);
+				} else
+					throw new Error("Could not load " + url);
+			}
+		};
+		xhr.send(null);
+	}
+
+}(this))
